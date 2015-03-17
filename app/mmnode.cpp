8c05f7f6989a419ed35482d69a5a733722febb23
Sandeep Datta <datta.sandeep@gmail.com>
2015-03-17 19:01:20 +0530

    Moved more code to MmNodeWidget::init().

--- app/mmnode.cpp
+++ app/mmnode.cpp
@@ -10,50 +10,40 @@ static const int YMARGIN = 27;
 
 MmNodeWidget::MmNodeWidget(QWidget *parent)
     : QWidget(parent)
-    , m_xMargin(XMARGIN)
-    , m_yMargin(YMARGIN)
-    , m_pHBox(new QHBoxLayout())
-    , m_pVBox(new QVBoxLayout())
-    , m_pLabel(new QLabel())
 {
     init("", sm_lastId + 1);
 }
 
 MmNodeWidget::MmNodeWidget(QString text, QWidget *parent)
     : QWidget(parent)
-    , m_xMargin(XMARGIN)
-    , m_yMargin(YMARGIN)
-    , m_pHBox(new QHBoxLayout())
-    , m_pVBox(new QVBoxLayout())
-    , m_pLabel(new QLabel())
-
 {
     init(text, sm_lastId + 1);
 }
 
-
 MmNodeWidget::MmNodeWidget(QString text, int id, QWidget *parent)
-    : m_id(id)
-    , QWidget(parent)
-    , m_xMargin(XMARGIN)
-    , m_yMargin(YMARGIN)
-    , m_pHBox(new QHBoxLayout())
-    , m_pVBox(new QVBoxLayout())
-    , m_pLabel(new QLabel())
+    :QWidget(parent)
 {
     init(text, id);
 }
 
-
 void MmNodeWidget::init(QString text, int id)
 {
-    m_pLabel->setText(text);
+    m_xMargin = XMARGIN;
+    m_yMargin = YMARGIN;
+    m_pHBox = new QHBoxLayout();
+    m_pVBox = new QVBoxLayout();
+    m_pLabel = new QLabel();
+    m_id = id;
     sm_lastId = max(sm_lastId, id);
-    setStyleSheet("border: 1px solid red");
-    setLayout(m_pHBox);
+
+    m_pLabel->setText(text);
+
     m_pHBox->addWidget(m_pLabel);
     m_pHBox->addSpacing(m_xMargin);
     m_pHBox->addLayout(m_pVBox);
+
+    setStyleSheet("border: 1px solid red");
+    setLayout(m_pHBox);
 }
 
 MmNodeWidget::~MmNodeWidget()
